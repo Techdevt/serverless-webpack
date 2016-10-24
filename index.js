ae939a1806d77cce9a4c484bf0cf4b907d85453d
Benjamin Appiah-Brobbey <appiahbrobbeybenjamin@rocketmail.com>
2016-10-24 09:13:49 +0000

    add hook to compile before sls offline run

--- index.js
+++ index.js
@@ -117,6 +117,13 @@ class ServerlessWebpack {
       'webpack:serve:serve': () => BbPromise.bind(this)
         .then(this.validate)
         .then(this.serve),
+      'before:offline:start:init': () => BbPromise.bind(this)
+        .then(this.validate)
+        .then(this.compile),
+      
+      'before:offline:start': () => BbPromise.bind(this)
+        .then(this.validate)
+        .then(this.compile),
     };
   }
 }
